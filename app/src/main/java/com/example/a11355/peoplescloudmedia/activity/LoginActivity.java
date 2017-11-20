fa074c8d70eebb705dfcd5f0e5bd102a96780604
Sam <1135520344@qq.com>
2017-11-21 00:27:43 +0800

    即将修改登录注册也

---
+++ app/src/main/java/com/example/a11355/peoplescloudmedia/activity/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.a11355.peoplescloudmedia.activity;
+
+import com.example.a11355.peoplescloudmedia.R;
+import com.example.a11355.peoplescloudmedia.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+
+    @Override
+    protected int getViewResId() {
+        return R.layout.activity_login;
+    }
+}
